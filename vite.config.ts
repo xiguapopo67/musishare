8a84257278a41d0de8d750e7c1cbb315b3a0f670
刘茜 <xiguapopo67@163.com>
2025-08-29 21:16:33 +0800

    修改主页路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/melon-music-share/',
+  base: '/',
   server: {
     port: 3000,
     host: true
